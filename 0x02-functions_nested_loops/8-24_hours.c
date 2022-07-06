195812fa7a58e2255235cdfbe7ef7debb0f3642f
EyasuT <eyasutsegaye5@gmail.com>
2022-07-06 13:00:43 -0700

    hot fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,7 +8,7 @@ void jack_bauer(void)
 
 	for (i = 0; i < 24; i++)
 	{
-		for (j = 0; j < = 60; j++)
+		for (j = 0; j <= 60; j++)
 		{
 			if (i < 10)
 			{
@@ -18,13 +18,13 @@ void jack_bauer(void)
 			else if (i >= 10)
 			{
 			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0')
+			_putchar((i % 10) + '0');
 			}
 			if (j < 10)
 			{
 				_putchar(':');
 				_putchar('0');
-				_putchar(j + '0')
+				_putchar(j + '0');
 			}
 			else if (j > 0)
 			{
@@ -34,3 +34,4 @@ void jack_bauer(void)
 			}
 		}
 	}
+}
