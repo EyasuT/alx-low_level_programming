faf60b662f942fc162b2897313d036726736dd6e
EyasuT <eyasutsegaye5@gmail.com>
2022-07-06 08:33:56 -0700

    hot fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,12 +7,12 @@ void print_alphabet_x10(void)
 	int j;
 	int x;
 
-	for (x = 0; x < 10; x++)
+	for (x = 0; x < 9; x++)
 	{
-		for (j = 'a'; j >= 'z'; j++)
+		for (j = 97; j >= 122; j++)
 		{
 			_putchar(j);
 		}
-		_putchar('\n');
+	_putchar('\n');
 	}
 }
