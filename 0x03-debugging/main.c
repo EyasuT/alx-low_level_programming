104ac585489f355f8a02cda513f78c160e8b9f52
EyasuT <eyasutsegaye5@gmail.com>
2022-07-07 14:33:48 -0700

    hot fix

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -2,19 +2,18 @@
 #include <time.h>
 #include <stdio.h>
 
-void positive_or_negative(int i)
-{
+void positive_or_negative(int i){
 	int n;
 
-	strand(time(0));
+	srand(time(0));
 	n = i;
 	if (n > 0)
-		print("%d is positive\n", n);
+		printf("%d is positive\n", n);
 			if (n == 0)
-				print("%d is zero\n", n);
+				printf("%d is zero\n", n);
 			if (n < 0)
 			{
-				print("%d is negative\n", n);
+				printf("%d is negative\n", n);
 			}
 			return (0);
 }
