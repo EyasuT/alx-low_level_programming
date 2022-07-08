f28f5ec12cfbf3b2bf4e3e4c6b6280b32cd39d7b
EyasuT <eyasutsegaye5@gmail.com>
2022-07-08 06:13:01 -0700

    checking upper case letter

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isupper - this is a function to check uppercase alphas
+ * _isupper -> this is a function to check uppercase alphas
  * @c: an integer paramer
  * Return: something but success
  */
