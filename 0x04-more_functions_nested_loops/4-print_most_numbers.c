540bb426de99e966f91861f2e73189d20312705b
EyasuT <eyasutsegaye5@gmail.com>
2022-07-08 08:09:53 -0700

    print numbers done

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - this prints most numbers
+ */
+
+void print_most_numbers(void);
+{
+	int X;
+
+	for (X = 0; X < 10; X++)
+	{
+		if (X != 0 && X != 4)
+			_putchar(X + '0');
+	}
+	_putchar('\n');
+}
