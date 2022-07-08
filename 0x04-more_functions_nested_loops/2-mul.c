16d9e115038106ab1da39e4804f49d153694eda7
EyasuT <eyasutsegaye5@gmail.com>
2022-07-08 07:05:51 -0700

    multiplying is done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul: multiplying the result of two parameters
+ * @a: int param 1
+ * @b: int param 2
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
