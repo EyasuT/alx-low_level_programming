cc4bb17e663234021da2e98d356ef1e9843e39c7
EyasuT <eyasutsegaye5@gmail.com>
2022-07-08 15:03:14 -0700

    print integer done

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,16 +1,19 @@
 #include "main.h"
-
 /**
- * print_most_numbers - this prints most numbers
+ * print_most_numbers - print the numbers, from 0 to 9, followed by new line
+ * Return: 0
  */
 void print_most_numbers(void)
 {
-	int X;
+	char <= 0;
 
-	for (X = 0; X < 10; X++)
+	while (n <= 9)
 	{
-		if (X != 0 && X != 4)
-			_putchar(X + '0');
+		if (n != 2 && n != 4)
+		{
+			_putchar('0' + n);
+		}
+		n++;
 	}
 	_putchar('\n');
 }
