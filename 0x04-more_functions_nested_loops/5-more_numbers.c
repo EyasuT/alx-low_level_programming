edd93dc50ce579e59dbb4fd24e3171433667cb15
EyasuT <eyasutsegaye5@gmail.com>
2022-07-08 09:27:17 -0700

    print more numbers done

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * more_numbers -> print more numbers
+ */
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 1o; x++)
+	{
+		for (y = 0, y < 15, y++)
+		{
+			_putchar(y + '0');
+		}
+		_putchar('\n');
+	}
+}
