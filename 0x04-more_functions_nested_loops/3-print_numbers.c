9a280a87549e6008da46daca5658918fb148a79f
EyasuT <eyasutsegaye5@gmail.com>
2022-07-08 07:39:16 -0700

    multiplication done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print numbers -> print numbers from 0 to 9 followed to new line
+ * Returns: 0
+ */
+
+void print_numbers(void)
+{
+	int X;
+
+	for (X = 0; X < 10; X++)
+	       _putchar(X + '0');
+	_putchar('\n');
+}
