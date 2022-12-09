000364c44f54d86299abafa354696f639d97c430
EyasuT <eyasutsegaye5@gmail.com>
2022-12-09 14:09:57 -0800

    alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 /**
-* main - assign a random number to the variable n each time it is executed
-* and print the last digit of the number stored in the variable n
-* Return: always 0
+* main - Prints the alphabet in lower case
+*
+* Return: Always 0 (Success)
 */
 int main(void)
-{
-	int ch;
 
-	for (ch = 97; ch = 122; ch++)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
 	{
-		putchar(ch);
+		putchar (lc);
 	}
-	putchar(10); /* this is an ASCII code for new line*/
+	putchar ('\n');
 	return (0);
 }
