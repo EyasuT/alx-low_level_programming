ce6c54e1ff981d31f19dd8e7a48fbbc028c2951f
EyasuT <eyasutsegaye5@gmail.com>
2022-12-12 05:01:05 -0800

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 /**
-*assign a random number to the variable n each time it is executed
-*and print the last digit of the number stored in the variable n 
-*Return : always 0
-*/
-
+ * main - prints combinations of two two-digit numbers.
+ * Return: Always 0
+ */
 int main(void)
 {
 	int ch;
 	int n;
+
 	for(ch = 48; ch <= 57; ch++)
 	{
 		for(n = 48; n <= 57; n++)
