bfdb8c0d947f37542a9a422e1b30df1f55cf8e64
EyasuT <eyasutsegaye5@gmail.com>
2022-12-12 03:19:43 -0800

    task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
-/**                                                                                                                                     
-*assign a random number to the variable n each time it is executed                                                                  
-*and print the last digit of the number stored in the variable n                                                                    
-*Return : always 0                                                                                                                   
-*/
-
+/**
+ *  main - print the alphabet in reverse
+ *  Return: Always 0
+ */
 int main(void)
 {
-	char ch;
-	for (ch = 'z'; ch >= 'a'; ch--)
-	{
-		putchar(ch);
-	}
-	putchar(10); /* this is an ASCII code*/
+	int le;
+
+	for ('z'; le >= 'a'; le--)
+	putchar(le);
+
+	putchar('\n');
 	return (0);
 }
