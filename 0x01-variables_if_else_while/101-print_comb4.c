81de0d49443d10bbdf1564fb56d7facd02f7ca09
EyasuT <eyasutsegaye5@gmail.com>
2022-12-12 04:20:54 -0800

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
-*assign a random number to the variable n each time it is executed
-*and print the last digit of the number stored in the variable n
-*Return : always 0 
+* main - prints combination of three digit
+* Return: always 0 
 */
 
 int main(void)
@@ -10,6 +9,7 @@ int main(void)
 	int ch;
 	int n;
 	int m;
+
 	for (ch = 48; ch <= 57; ch++)
 	{
 		for (n = 49; n <= 57; n++)
@@ -34,5 +34,3 @@ int main(void)
 	putchar(10); /*this is an ASCII code*/
 	return (0);
 }
-
-
