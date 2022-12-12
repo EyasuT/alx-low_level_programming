38a8fa8cdd07887c12609f8d9295d02162cf6ef0
EyasuT <eyasutsegaye5@gmail.com>
2022-12-12 03:38:55 -0800

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,27 +1,16 @@
-#include <stdio.h> 
+#include <stdio.h>
 /**
-*assign a random number to the variable n each time it is executed 
-*and print the last digit of the number stored in the variable n 
-*Return : always 0
-*/
-
+ * main - prints all the numbers of base 16 in lowercase
+ * Return: Always 0
+ */
 int main(void)
 {
-/**
- *
- * hexadecimal
- * 0 1 2 3 4 5 6 7 8 9 a b c d e f 
-*/
-	char ch;
-	int n;
-	for(n = 48; n <= 57; n++)
-	{
-		putchar(n);
-	}
-	for (ch = 'a'; ch <= 'f'; ch ++)
-	{
-		putchar(ch);
-	}
-	putchar(10);
+	int num;
+	char la;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	for (la = 'a'; la < 'f'; la++)
+		putchar(la);
 	return (0);
 }
