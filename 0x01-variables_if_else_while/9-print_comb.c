a7ddacd60c6528d166cbc9239663260d26faa953
EyasuT <eyasutsegaye5@gmail.com>
2022-12-12 03:53:55 -0800

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
-*assign a random number to the variable n each time it is executed
-*and print the last digit of the number stored in the variable n
-*Return : always 0
+* main - prints all combination of single digits
+* Return: Always 0
 */
 int main(void)
 {
