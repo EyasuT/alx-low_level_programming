aacdd87ec83eb576463f5cf619c9465d5cf86833
EyasuT <eyasutsegaye5@gmail.com>
2022-12-12 05:03:30 -0800

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,15 +8,15 @@ int main(void)
 	int ch;
 	int n;
 
-	for(ch = 48; ch <= 57; ch++)
+	for (ch = 48; ch <= 57; ch++)
 	{
-		for(n = 48; n <= 57; n++)
+		for (n = 48; n <= 57; n++)
 		{
-			if(n > ch)
+			if (n > ch)
 			{
 				putchar(ch);
 				putchar(n);
-				if(ch != 57 || n != 57)
+				if (ch != 57 || n != 57)
 				{
 					putchar(44);
 					putchar(32);
@@ -25,5 +25,5 @@ int main(void)
 		}
 	}
 	putchar (10); /*this is an ASCII code*/
-	return(0);
+	return (0);
 }
