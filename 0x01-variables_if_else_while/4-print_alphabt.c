3ba01d9a61dc2f522432b71e0e8af55a77da10e3
EyasuT <eyasutsegaye5@gmail.com>
2022-12-12 02:41:06 -0800

    task 5

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 /**
-* main : Prints the alphabet in lowercase
-* followed by a new line, except q and e
-* Return: always 0 (sucess)
+* main : Prints the alphabet without q and e.
+* Return: Always 0
 */
 int main(void)
 {
-	char (ch);
-	for (ch = 'a'; ch <= 'z'; ch++)
+	char i;
+
+	for (i = 97; i <= 123; i++)
 	{
-		if (ch != 'q' && ch != 'e')
+		if (i != 101 && ch != 113)
 		{
-			putchar (ch);
+			putchar (i);
 		}
 	}
-	putchar (10);
+	putchar ('\n');
 	return (0);
 }
--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,19 +1,14 @@
-#include <stdio.h>                                                                                                                      
-
-/**                                                                                                                          
-*assign a random number to the variable n each time it is executed                                                                   
-*and print the last digit of the number stored in the variable n                                                                     
-*Return : always 0                                                                                                                   
-*/
-int main (void)
+#include <stdio.h>
+/**
+ *  main - Prints numbers from 0 to 9.
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
 {
-	int n;
-	for (n = 0; n < 10; n++)
-	{
-		printf("%d\n", n);
-	}
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
 	printf("\n");
 	return (0);
 }
-
-
