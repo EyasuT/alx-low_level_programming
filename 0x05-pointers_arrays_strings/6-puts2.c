f03f2db0c49000118c7d3681478726087f5b1e2c
EyasuT <eyasutsegaye5@gmail.com>
2022-12-20 12:20:52 -0800

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,12 +7,12 @@
  */
 void puts2(char *str)
 {
-	int n = 0;
+	int i = 0;
 
-	for (; str[n] != '\0'; n++)
+	for (; str[i] != '\0'; i++)
 	{
-		if ((n % 2) == 0)
-			_putchar(str[n]);
+		if ((i % 2) == 0)
+			_putchar(str[i]);
 		else
 			continue;
 	}
