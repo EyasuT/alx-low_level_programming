dc98677756b5b948caff91fe7d37cb7938b8ad89
EyasuT <eyasutsegaye5@gmail.com>
2023-01-09 13:11:43 -0800

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,18 +11,16 @@
  */
 int check_num(char *str)
 {
- unsigned int count;
+	unsigned int count;
 
- count = 0;
- while (count < strlen(str))
- {
- if (!isdigit(str[count]))
- {
- return (0);
- }
- count++;
- }return (1);
- }
-
-
-
+	count = 0;
+	while (count < strlen(str))
+{
+	if (!isdigit(str[count]))
+	{
+		return (0);
+	}
+	count++;
+}
+	return (1);
+}
